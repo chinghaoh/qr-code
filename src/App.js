a1f6cf9e239d10bdff2ee45ef7d679a8b8e120d4
Chinghaogit config --global user.email chinghao.hsu@hva.nlgit config --global user.name <chinghaoh@hotmail.nl>
2024-01-20 17:34:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./styles.css"
+import qrImage from "./images/image-qr-code.png"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card-container">
+      <div className="card">
+      <img className='qr-image' src={qrImage} alt='qr-code'></img>
+      <div className='card-text'>
+        <h2 id='card-title'>Improve your front-end skills by building projects</h2>
+        <p id='card-content'>Scan the GR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
