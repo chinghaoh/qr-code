bbd4ba1ae2ccce2088b80af6e5686880adb0767e
Chinghaogit config --global user.email chinghao.hsu@hva.nlgit config --global user.name <chinghaoh@hotmail.nl>
2024-01-21 13:41:33 +0100

    changed header level and added main body

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import qrImage from "./images/image-qr-code.png"
 
 const App = () => {
   return (
-    <div className="card-container">
+    <main className="card-container">
       <div className="card">
       <img className='qr-image' src={qrImage} alt='qr-code'></img>
       <div className='card-text'>
-        <h2 id='card-title'>Improve your front-end skills by building projects</h2>
+        <h1 id='card-title'>Improve your front-end skills by building projects</h1>
         <p id='card-content'>Scan the GR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 export default App;
